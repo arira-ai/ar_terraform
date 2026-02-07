3f1c6ef71f5d2bcfb74cb5867e5b44a1bc3f79b0
Arira_AI <arira.aizen@gmail.com>
2026-02-07 06:16:49 +0530

    Update S3 bucket name in backend configuration

--- 07-Hands-On/01-basics/02-WIth_Backend/backend.tf
+++ 07-Hands-On/01-basics/02-WIth_Backend/backend.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "jhonwick-terraform-state"
+    bucket         = "ar-terraform-state"
     key            = "ec2/dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
 }
+
