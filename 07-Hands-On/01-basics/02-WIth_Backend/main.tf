2558e26f16315d07d9c482b41743d0175fab5f0d
Arira_AI <arira.aizen@gmail.com>
2026-02-07 06:41:13 +0530

    Add AWS provider and DynamoDB table for state locking
    Added AWS provider configuration and DynamoDB table resource for state locking.

--- 07-Hands-On/01-basics/02-WIth_Backend/main.tf
+++ 07-Hands-On/01-basics/02-WIth_Backend/main.tf
@@ -18,3 +18,25 @@ resource "aws_instance" "demo" {
     Env  = "dev"
   }
 }
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "terraform-lock-table"
+    Purpose = "Terraform state locking"
+  }
+}
+
+
