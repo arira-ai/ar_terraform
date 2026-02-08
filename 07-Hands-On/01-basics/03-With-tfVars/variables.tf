219fd075c83ce923528036e005d996ed6dba962c
Arira_AI <arira.aizen@gmail.com>
2026-02-08 18:14:06 +0530

    Add Terraform variables for AWS configuration

---
+++ 07-Hands-On/01-basics/03-With-tfVars/variables.tf
@@ -0,0 +1,27 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "owner" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "ami_name_pattern" {
+  type = string
+}
